db1fc1293f58345eaad07e7b33aead2682294b18
bianca <bianca.g.buha@gmail.com>
2023-03-21 11:47:27 +0000

    task1

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 const button = document.querySelector("#click-me");
-
+// Task 1
 function handleClick(event) {
   // If clicked, console log you clicked me!
   // If shift key pressed, change the inner text of the button to be NAILED IT
+  // find property for Shift Key
+  console.log('You clicked me!')
+  console.log(event)
+
+  // if Shift key then 
+  if ( event.shiftKey) {
+    button.textContent = "NAILED IT!";
+    console.log(event) 
+  }
+  // console.log(event)
 }
 
 button.addEventListener("click", handleClick);
